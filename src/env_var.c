c0d61961f7508898b5927482883ea5f7e684fbdf
avaldin <valdinalan.pro@gmail.com>
2024-04-08 16:08:17 +0200

    ambigous var and red union to do and its done for redirection

--- src/env_var.c
+++ src/env_var.c
@@ -6,22 +6,33 @@
 /*   By: avaldin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 11:20:59 by avaldin           #+#    #+#             */
-/*   Updated: 2024/04/08 13:56:24 by avaldin          ###   ########.fr       */
+/*   Updated: 2024/04/08 15:53:58 by avaldin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
+
+//int	ambigous_var(t_red *red, char **env, int i, int j)
+//{
+//	char	*var;
+//	int 	len_name;
+//
+//	len_name = 0;
+//	while ((token[*i + len_name + 1] < 91 && token[*i + len_name + 1] > 64) || (token[*i + len_name + 1] < 123 && token[*i + len_name + 1] > 96) || (token[*i + len_name + 1] < 58 && token[*i + len_name + 1] > 47))
+//		len_name++;
+//	find_var(red->temp[i][j], env, )
+//}
 
 char	*find_var(char *name, char **env, int len)
 {
 	int	i;
 
 	i = 0;
-	while (ft_strncmp(name, env[i], len))
+	while (env[i] && ft_strncmp(name, env[i], len))
 		i++;
 	if (!env[i] || !env[i][len + 1])
 		return (NULL);
-	return (ft_strdup(name, len + 1, -1));
+	return (ft_strdup(env[i], len + 1, -1));
 }
 
 char	*apply_var(char *token, char **env, int *i)
@@ -34,11 +45,11 @@ char	*apply_var(char *token, char **env, int *i)
 		len_name++;
 	if (!len_name)
 		return ((*i)++, token);
-	var = find_var(&token[*i], env, len_name);
+	var = find_var(&token[*i + 1], env, len_name);
 	if (!var)
 		return (str_cut(token, *i, *i + len_name));
 	*i += ft_strlen(var);
-	str_modify(token, *i - ft_strlen(var), len_name + 1, var);
+	token = str_modify(token, *i - ft_strlen(var), len_name + 1, var);
 	return (token);
 }
 
@@ -52,8 +63,11 @@ void	check_var(t_red *red, char **env, int i)
 		if (red->temp[i][j] == '$')
 		{
 			if (!red->temp[i][j + 1] && red->protection[i + 1] != 0 && red->protection[i] == 0)
-				return ;
-			red->temp[i] = apply_var(red->temp[i], env, &j);
+				red->temp[i] = str_cut(red->temp[i], j, j + 1);
+			else if (ambigous_var(red, env, i))
+				return ; // pas ok, faut gerer ca
+			else
+				red->temp[i] = apply_var(red->temp[i], env, &j);
 		}
 		else
 			j++;
@@ -72,10 +86,10 @@ void	process_var(t_section *first, char **env)
 		red = sect->first_red;
 		while (red)
 		{
-			i = 0;
-			while (red->temp[i])
+			i = -1;
+			while (red->temp[++i])
 				if (red->protection[i] != 2)
-					check_var(red, env, i++);
+					check_var(red, env, i);
 			red = red->next;
 		}
 		sect = sect->next;
